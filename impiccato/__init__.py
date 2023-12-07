70b32cf546beb7bb3e9cf9152fd28260246f78ff
CarlBishop <peppino@galad.it>
2023-12-07 11:47:01 +0100

    Update __init__.py

--- impiccato/__init__.py
+++ impiccato/__init__.py
@@ -3,4 +3,8 @@ from .impiccato import Impiccato
 __red_end_user_data_statement__ = 'This cog does not store user data.'
 
 async def setup(bot):
-	await bot.add_cog(Impiccato(bot))
+    cog = Impiccato(bot)
+    r = bot.add_cog(cog)
+    if r is not None:
+        await r
+        await cog.initialize()

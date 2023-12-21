e8a5b7a5784215fa800f6ff5eb5a0195a3ad9e5c
CarlBishop <peppino@galad.it>
2023-12-21 14:27:20 +0100

    Update word_listener.py

--- wordlistener/word_listener.py
+++ wordlistener/word_listener.py
@@ -5,16 +5,16 @@ class WordListener(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.config = Config.get_conf(self, identifier=1234567890, force_registration=True)
-        self.config.register_guild(words=[])
+        self.config.register_guild(words={})
 
     async def check_word(self, message):
         guild_words = await self.config.guild(message.guild).words()
         if not guild_words:
             return False
 
-        for word in guild_words:
+        for word, embed_data in guild_words.items():
             if word.lower() in message.content.lower():
-                return True
+                return word, embed_data
         return False
 
     @commands.Cog.listener()
@@ -22,46 +22,32 @@ class WordListener(commands.Cog):
         if message.author.bot:
             return
 
-        if await self.check_word(message):
-            guild_words = await self.config.guild(message.guild).words()
-            if not guild_words:
-                return
-            
-            matched_word = None
-            for word in guild_words:
-                if word.lower() in message.content.lower():
-                    matched_word = word
-                    break
-            
-            if matched_word:
-                responses = {
-                    'baciccio': discord.Embed(title="Parola Monitorata Trovata", description=f"Trovata parola: {matched_word}. Ora scrivo del testo"),
-                    'ciccio': discord.Embed(title="Risposta per Parola 2", description="Descrizione per la Parola 2"),
-                    # Aggiungi altre risposte per parole aggiuntive
-                }
-                
-                if matched_word in responses:
-                    embed = responses[matched_word]
-                    author = message.author
-                    if isinstance(author, discord.Member):
-                        if author.avatar:
-                            embed.set_footer(text=f"Autore: {author.display_name}", icon_url=author.avatar.url)
-                        else:
-                            embed.set_footer(text=f"Autore: {author.display_name}")
+        word_data = await self.check_word(message)
+        if word_data:
+            word, embed_data = word_data
+            if embed_data:
+                embed = discord.Embed.from_dict(embed_data)
+                author = message.author
+                if isinstance(author, discord.Member):
+                    if author.avatar:
+                        embed.set_footer(text=f"Autore: {author.display_name}", icon_url=author.avatar.url)
                     else:
                         embed.set_footer(text=f"Autore: {author.display_name}")
-                    await message.channel.send(embed=embed)
+                else:
+                    embed.set_footer(text=f"Autore: {author.display_name}")
+                await message.channel.send(embed=embed)
 
     @commands.command()
     @commands.guild_only()
     @commands.admin()
-    async def add_word(self, ctx, word: str):
-        """Aggiunge una parola/frase da monitorare."""
+    async def add_word(self, ctx, word: str, *, embed_data: str):
+        """Aggiunge una parola/frase da monitorare con un embed associato."""
         word = word.lower()
         async with self.config.guild(ctx.guild).words() as guild_words:
             if word not in guild_words:
-                guild_words.append(word)
-                await ctx.send(f"La parola '{word}' è stata aggiunta alla lista di parole monitorate.")
+                embed_dict = eval(embed_data)  # Converti i dati dell'embed in un dizionario
+                guild_words[word] = embed_dict
+                await ctx.send(f"La parola '{word}' è stata aggiunta alla lista di parole monitorate con l'embed associato.")
             else:
                 await ctx.send(f"La parola '{word}' è già presente nella lista.")
 
@@ -73,7 +59,7 @@ class WordListener(commands.Cog):
         word = word.lower()
         async with self.config.guild(ctx.guild).words() as guild_words:
             if word in guild_words:
-                guild_words.remove(word)
+                del guild_words[word]
                 await ctx.send(f"La parola '{word}' è stata rimossa dalla lista di parole monitorate.")
             else:
                 await ctx.send(f"La parola '{word}' non è presente nella lista.")
@@ -85,7 +71,7 @@ class WordListener(commands.Cog):
         """Mostra la lista delle parole/frasi monitorate."""
         guild_words = await self.config.guild(ctx.guild).words()
         if guild_words:
-            word_list = "\n".join(guild_words)
+            word_list = "\n".join(guild_words.keys())
             await ctx.send(f"Parole/frasi monitorate:\n{word_list}")
         else:
             await ctx.send("Nessuna parola/frase è attualmente monitorata.")

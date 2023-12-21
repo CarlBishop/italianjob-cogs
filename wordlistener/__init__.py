0d1f0bf86f356d9bbe3027d8101efe2bf3d47fbf
CarlBishop <peppino@galad.it>
2023-12-21 15:13:38 +0100

    Update __init__.py

--- wordlistener/__init__.py
+++ wordlistener/__init__.py
@@ -4,3 +4,4 @@ __red_end_user_data_statement__ = 'This cog does not store user data.'
 
 async def setup(bot):
 	await bot.add_cog(WordListener(bot))
+	bot.add_command(WordListener.wordlistener_help)
